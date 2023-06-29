0ecf06e2ecf7c17c07f5e207bcf9eb73042feb5a
Mikhail Dushechkin <mishan92097@gmail.com>
2023-06-29 15:28:22 +0300

    fix: fix auth 2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,25 +8,36 @@ const { jwtSecret, modeProduction } = require('../utils/config');
 
 // eslint-disable-next-line
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  // const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   return next(new UnAuthorizedError(Message.BAD_AUTH));
+  // }
+
+  // const token = authorization.replace('Bearer ', '');
+
+  // let payload;
+
+  // try {
+  //   payload = jwt.verify(
+  //     token,
+  //     NODE_ENV === modeProduction ? JWT_SECRET : jwtSecret,
+  //   );
+  // } catch (err) {
+  //   return next(new UnAuthorizedError(Message.BAD_AUTH));
+  // }
+
+  // req.user = payload;
+  const token = req.cookies.jwt;
+  if (!token) {
     return next(new UnAuthorizedError(Message.BAD_AUTH));
   }
-
-  const token = authorization.replace('Bearer ', '');
-
   let payload;
-
   try {
-    payload = jwt.verify(
-      token,
-      NODE_ENV === modeProduction ? JWT_SECRET : jwtSecret,
-    );
+    payload = jwt.verify(token, NODE_ENV === modeProduction ? JWT_SECRET : jwtSecret);
   } catch (err) {
     return next(new UnAuthorizedError(Message.BAD_AUTH));
   }
-
   req.user = payload;
   next();
 };
